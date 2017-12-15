3f4cb2a84f6166356ec03f6ff6319e07bc638b73
Mark Pareja <Markiled@users.noreply.github.com>
2017-12-15 10:13:33 -0800

    script change
    test

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ function copyEmail(element) {
 	var $temp = $("<input>");
 	$("body").append($temp);
 	$temp.val($(element).text()).select();
-	document.execCommand("Copy");
-	alert("Copied to clipboard: " + $temp);
+	document.execCommand("copy");
 	$temp.remove();
 }
