8739aa135617fa31b2505a2aff50318b7a1045e7
Mark Pareja <Markiled@users.noreply.github.com>
2018-10-05 23:45:55 -0700

    Color Changes
    Background and fonts

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 // Animate Smooth Scroll
-$('#whats-new').on('click', function() {
-	const images = $('#images').position().top;
-	
-	$('html, body').animate(
-		{
-			scrollTop: images
-		},
-		900
-	);
+$('#view-work').on('click', function() {
+  const images = $('#images').position().top;
+
+  $('html, body').animate(
+    {
+      scrollTop: images
+    },
+    900
+  );
 });
