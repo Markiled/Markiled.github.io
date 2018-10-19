c20d7e004c0e3ff7a106b93adea70cf62d5f7aa0
Mark Pareja <Markiled@users.noreply.github.com>
2018-10-18 17:31:03 -0700

    Scroll To Top Test
    test

--- main.js
+++ main.js
@@ -1,11 +1,34 @@
 // Animate Smooth Scroll
 $('#view-work').on('click', function() {
-  const images = $('#images').position().top;
+	const images = $('#images').position().top;
 
-  $('html, body').animate(
+	$('html, body').animate(
     {
-      scrollTop: images
+		scrollTop: images
     },
     900
-  );
+	);
+});
+
+$(document).ready(function() {
+	$(window).scroll(function()
+	{
+		if($(this).scrollTop()>100)
+		{
+			$('#scroll').fadeIn();
+		}
+		else
+		{
+			$('#scroll').fadeOut();
+		}
+	});
+		$('#scroll').click(function()
+		{
+			$('html, body').animate(
+			{
+				scrollTop: 0
+			}
+			600);
+			return false;
+		});
 });
